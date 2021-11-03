1d66c6c6d70883cdc78219609f71bc09d72edab4
Anusha SP <anushaspga@gmail.com>
2021-11-03 23:34:43 +0530

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import quizService from "./quizService";
 import "./assets/style.css";
 
+
 class ReactJSPractice extends Component{
+    state ={
+        questionBank : []
+    };
+    getQuestions = () =>{
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            });
+        });
+    };
+    componentDidMount(){
+        this.getQuestions();
+    }
     render(){
         return(
             <div className="container">
                 <div className="title">QuizBee</div>
+                {this.state.questionBank.length > 0 && this.state.questionBank.map(({question,answers,
+                correct, questionId}) => (<h4>{question}</h4>))}
             </div>
         );
     }
